17d208ddd363d1940e65f6753cc9a3c5dcbf29a9
Swapnil Pande <swapnil.pande98@gmail.com>
2025-03-07 13:48:42 -0500

    source local/bin/env to use uv immediately

--- install.sh
+++ install.sh
@@ -11,9 +11,11 @@ mkdir -p "$EFR_HOME"
 if ! command -v uv &>/dev/null; then
     echo "==> uv not found. Installing uv..."
     curl -LsSf https://astral.sh/uv/install.sh | sh
+    source $HOME/.local/bin/env
 else
     echo "==> uv is already installed."
 fi
+
 
 # Check if virtual environment already exists
 if [ -d "$VENV_DIR" ]; then
